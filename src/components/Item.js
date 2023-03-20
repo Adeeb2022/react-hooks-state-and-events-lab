7f8dd6ee8189f1238ef6bd82b085c23b9f5f76d3
Adeeb2022 <adeebsamad01@gmail.com>
2023-03-20 19:15:27 +0300

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setCart] = useState(false);
+
+  const addItemToCart = () => {
+    setCart(true);
+  };
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addItemToCart} className={inCart ? "remove" : "add"}>
+      {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
